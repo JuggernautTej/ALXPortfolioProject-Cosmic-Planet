25dcb33a39aeca25cf425b1313c6d56e4adf6f6c
JuggernautTej <tiwatej@gmail.com>
2024-10-12 14:40:10 +0100

    began adding styling to each route. About page was not rendering but is now fixed by adding the site description to the route and editing the ejs script.

--- server/routes/main.js
+++ server/routes/main.js
@@ -12,7 +12,11 @@ router.get('', (req, res) => {
 });
 
 router.get('/about', (req, res) => {
-    res.render('about');
+    const siteDesc = {
+        title: "Cosmic Planet",
+        description: " A blog site where creativity meets the universe. "
+    }
+    res.render('about', { siteDesc });
 });
 
 
