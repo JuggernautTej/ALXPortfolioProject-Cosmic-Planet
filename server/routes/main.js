bb16b574746ca3ea243e7c51abf1d5ff9e47347f
JuggernautTej <tiwatej@gmail.com>
2024-10-14 12:38:33 +0100

    configured the pagination for the blog posts in the home page and the main route

--- server/routes/main.js
+++ server/routes/main.js
@@ -7,13 +7,28 @@ const router = Router();
 // Routes
 // GET method for home route
 router.get('', async (req, res) => {
-    const siteDesc = {
-        title: "Cosmic Planet",
-        description: " A blog site where creativity meets the universe. "
-    }
     try {
-        const data = await Post.find();
-        res.render('index', { siteDesc, data });    
+        const siteDesc = {
+            title: "Cosmic Planet",
+            description: " A blog site where creativity meets the universe. "
+        }
+        let perPage = 6; // change back to 10
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: { createdAt: -1 } }]) // this makes the oldest post be at the top
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', { 
+            siteDesc,
+            data,
+            current: page,
+            nextPage:  hasNextPage ? nextPage : null});    
     } catch (error) {
         console.log(error)
     }
